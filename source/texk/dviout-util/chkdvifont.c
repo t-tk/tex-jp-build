ee2d719640d69f4c28708754de70c7882728132a
TANAKA Takuji <ttk@t-lab.opal.ne.jp>
2026-02-04 22:58:10 +0900

    chkdvifont: header, trial

--- source/texk/dviout-util/chkdvifont.c
+++ source/texk/dviout-util/chkdvifont.c
@@ -65,6 +65,9 @@
 #define GCC 1
 #endif
 
+#include "dd.h"
+#include "common.h"
+
 #include <config.h>
 #ifdef KPATHSEA
 #include <kpathsea/config.h>
@@ -72,9 +75,6 @@
 #include <kpathsea/variable.h>
 #endif
 #endif
-
-#include "dd.h"
-#include "common.h"
 
 #if defined(WIN32) && defined(KPATHSEA)
 #undef fopen
